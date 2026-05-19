b666c4e03543d8a727636131d399ffc65dec37ec
jonathanramirez253631 <jonathanramirez253631@correo.itm.edu.co>
2026-05-18 19:07:45 -0500

    FoundedDate added in each team

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -6,6 +6,7 @@ using SportsLeague.Domain.Helpers;
 using SportsLeague.Domain.Interfaces.Repositories;
 using SportsLeague.Domain.Interfaces.Services;
 using SportsLeague.Domain.Services;
+using SportsLeague.DataAccess.Seeders;  // Agregar este using
 
 var builder = WebApplication.CreateBuilder(args);// contenedores de dependencias, y se van agregando a medida que se creen nuevas, en este momentos olo esta team
 
@@ -61,6 +62,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// ── Data Seeder ──
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider
+        .GetRequiredService<LeagueDbContext>();
+
+    await context.Database.MigrateAsync(); // Crea la BD + aplica migraciones
+    await DataSeeder.SeedAsync(context);
+}
+
 
 // ── Middleware Pipeline ──
 if (app.Environment.IsDevelopment())
